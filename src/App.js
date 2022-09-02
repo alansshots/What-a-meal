0565ebbb71249649e2f576512b70bedaeb542c37
alansshots <alengospodinov@gmail.com>
2022-09-02 18:10:22 +0300

    categories + smooth scroll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 
+import './App.css'
+
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Footer from './components/Footer';
+
 
 function App() {
   return (
--- src/components/Search.js
+++ src/components/Search.js
@@ -6,6 +6,7 @@ import Mealcard from './Mealcard'
 
 const Search = () => {
   const [search, setSearch] = useState('')
+  const [category, setCategory] = useState('')
   const [meals, setMeals] = useState([])
 
 
@@ -14,8 +15,7 @@ const Search = () => {
   }, [])
 
     const handleRandomSearch = async () => {
-    //   const { data } = await axios('https://www.themealdb.com/api/json/v1/1/random.php');
-      const { data } = await axios('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood');
+      const { data } = await axios('https://www.themealdb.com/api/json/v1/1/random.php');
       setMeals(data.meals)
       console.log(meals)
     }
@@ -26,65 +26,53 @@ const Search = () => {
       console.log(meals)
     }  
 
+    const handleSearchByCategory = async () => {
+        const { data } = await axios(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
+        setMeals(data.meals)
+        console.log(meals)
+      }  
+
   return (
     <>
-    <div id='search' className='w-1/2 m-auto mt-10 flex '>
-        <form className='w-full' >   
+    <div id='search' className='w-full lg:w-1/2 lg:m-auto mt-10 flex-col items-center justify-center mr-10'>
+        <form className='w-full m-5'>   
             <label htmlFor="search" className="mb-2 font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
             <div className="relative">
                 <div className="flex absolute inset-y-0 left-0 items-center pl-3 text-2xl">
                 <MdSearch/>
                 </div>
-                <input type="text" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" placeholder="Search whatever your belly is craving for..." onChange={(e)=> setSearch(e.target.value)} />
+                <input type="text" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" placeholder="Search..." onChange={(e)=> setSearch(e.target.value)} />
                 <button type="button" className="text-white absolute right-2.5 bottom-2.5 bg-red-600 hover:bg-red-700  font-medium rounded-lg text-sm px-4 py-2" onClick={handleSearch}>Search</button>
             </div>
         </form>
+        <div className='flex flex-row items-center justify-center'>
+        <button className="text-white bg-red-600 hover:bg-red-700 font-medium rounded-lg text-sm px-7 py-1 ml-5" 
+        onClick={() => { setCategory('Beef'); handleSearchByCategory();}}>Beef</button>
+        <button className="text-white bg-red-600 hover:bg-red-700 font-medium rounded-lg text-sm px-7 py-1 ml-5" 
+        onClick={handleSearchByCategory}>Breakfast</button>
+        <button className="text-white bg-red-600 hover:bg-red-700 font-medium rounded-lg text-sm px-7 py-1 ml-5" 
+        onClick={handleSearchByCategory}>Chicken</button>
+        <button className="text-white bg-red-600 hover:bg-red-700 font-medium rounded-lg text-sm px-7 py-1 ml-5" 
+        onClick={handleSearchByCategory}>Dessert</button>
+        <button className="text-white bg-red-600 hover:bg-red-700 font-medium rounded-lg text-sm px-7 py-1 ml-5" 
+        onClick={handleSearchByCategory}>Pasta</button>
+        <button className="text-white bg-red-600 hover:bg-red-700 font-medium rounded-lg text-sm px-7 py-1 ml-5" 
+        onClick={handleSearchByCategory}>Seafood</button>
         <button className="text-white bg-red-600 hover:bg-red-700 font-medium rounded-lg text-sm px-7 py-1 ml-5" 
         onClick={handleRandomSearch}>Surprise me!</button>
-    </div>
-        {/* <div className='flex flex-row items-center justify-center '>
-            <div className='m-4'>
-            <label for="underline_select" class="sr-only">Underline select</label>
-            <select  class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-200 peer">
-                <option selected>Choose a region</option>
-                <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                <option value="FR">France</option>
-                <option value="DE">Germany</option>
-            </select>
-            </div>  
-            <div className='m-4'>
-            <label for="underline_select" class="sr-only">Underline select</label>
-            <select  class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-200 peer">
-                <option selected>Choose a region</option>
-                <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                <option value="FR">France</option>
-                <option value="DE">Germany</option>
-            </select>
-            </div>
-
-            <div className='m-4'>
-            <label for="underline_select" class="sr-only">Underline select</label>
-            <select  class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none focus:outline-none focus:ring-0 focus:border-gray-200 peer">
-                <option selected>Choose a region</option>
-                <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                <option value="FR">France</option>
-                <option value="DE">Germany</option>
-            </select>
-            </div>
-        </div>  */}
-
-        <div id='results' className='flex flex-col items-center justify-center mt-10'>
-            <h2 className='text-red-600 text-4xl font-semibold mb-10'>Results</h2>
-            <div className='flex flex-row flex-wrap items-center justify-center'> 
-            {meals.map((meal) => (
-                <Mealcard meal={meal} key={meal.idMeal}/>
-            ))}
-            </div>
         </div>
-        </>
+        
+    </div>
+
+    <div id='results' className='flex flex-col items-center justify-center mt-10'>
+        <h2 className='text-red-600 text-4xl font-semibold mb-10'>Results</h2>
+        <div className='flex flex-row flex-wrap items-center justify-center'> 
+        {meals.map((meal) => (
+            <Mealcard meal={meal} key={meal.idMeal}/>
+        ))}
+        </div>
+    </div>
+    </>
   )
 }
 
